bef47beaa6e38700585c775ee5e64965d588fead
Steve Poll <stevepoll@gmail.com>
2020-06-14 10:14:31 -0400

    Handle error from AWS when stopping a pending instance or starting a stopping one

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -77,9 +78,19 @@ def create_snapshots(project):
   instances = filter_instances(project)
 
   for i in instances:
-    for v in volumes.all():
-      print("Creating snapshot of {0}".format(v.id))
+    print("Stopping {0}...".format(i.id))
+    i.stop()
+    i.wait_until_stopped()
+
+    for v in i.volumes.all():
+      print("  Creating snapshot of {0}".format(v.id))
       v.create_snapshot(Description='Created by snapshotalyzer 30000')
+    
+    print("Starting {0}...".format(i.id))
+    i.start()
+    i.wait_until_running()
+
+  print("Job's done!")
   # End create_snapshots
 
 @instances.command('list')
@@ -109,7 +120,11 @@ def stop_instances(project):
   
   for i in instances:
     print("Stopping {0}...".format(i.id))
-    i.stop()
+    try:
+      i.stop()
+    except botocore.exceptions.ClientError as e:
+      print("Could not stop {0}. ".format(i.id) + str(e))
+      continue
   # End stop_instances
 
 @instances.command('start')
@@ -120,7 +135,11 @@ def start_instances(project):
   
   for i in instances:
     print("Starting {0}...".format(i.id))
-    i.start()
+    try:
+      i.start()
+    except botocore.exceptions.ClientError as e:
+      print("Could not start {0}. ".format(i.id) + str(e))
+      continue
   # End start_instances
   
 
